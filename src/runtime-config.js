14dc01eb0136988918f2b75d42f5af32822604d5
Nick Kania <nickkania@gmail.com>
2026-02-08 17:20:17 -0500

    Update runtime config

--- src/runtime-config.js
+++ src/runtime-config.js
@@ -1,6 +1,1 @@
-window.__STEWARDSYNC_CONFIG__ = ${JSON.stringify({
-  convexUrl: typeof process !== 'undefined' && process.env?.PUBLIC_CONVEX_URL || 'http://127.0.0.1:3210',
-  enableDevLogin: typeof process !== 'undefined' && process.env?.PUBLIC_ENABLE_DEV_LOGIN === 'true' || false,
-  discordClientId: typeof process !== 'undefined' && process.env?.DISCORD_CLIENT_ID || '',
-  discordClientSecret: typeof process !== 'undefined' && process.env?.DISCORD_CLIENT_SECRET || ''
-})};
+window.__STEWARDSYNC_CONFIG__ = window.__STEWARDSYNC_CONFIG__ || {};
